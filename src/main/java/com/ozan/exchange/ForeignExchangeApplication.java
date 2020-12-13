d6d63c61f0b3f6e682ff965059224025879b84e1
mithatkonuk <mithatkonuk@gmail.com>
2020-12-13 23:34:11 +0300

    dev

--- src/main/java/com/ozan/exchange/ForeignExchangeApplication.java
+++ src/main/java/com/ozan/exchange/ForeignExchangeApplication.java
@@ -23,34 +23,34 @@ public class ForeignExchangeApplication
         SpringApplication.run(ForeignExchangeApplication.class, args);
     }
 
-//    @Component
-//    public static class Runner implements CommandLineRunner
-//    {
-//
-//        private static final Logger logger = LoggerFactory.getLogger("defaultErrorFileLogger");
-//        @Autowired
-//        @Qualifier( "${forgien_exchange_providers.feign.name}" )
-//        private ForgienExchangeProvider defaultProvider;
-//
-//        @Autowired
-//        @Qualifier( "${forgien_exchange_providers.rest.name}" )
-//        private ForgienExchangeProvider forgienExchangeProvider;
-//
-//        //        AllArgsConstructor is not capability to know which qualifier
-//        //        @Autowired
-//        //        public Runner( @Qualifier( "${forgien_exchange_providers.external.name}" ) ForgienExchangeProvider externalProvider) {
-//        //
-//        //        }
-//
-//        @Override
-//        public void run( String... args )
-//        {
-//            Exchange exchange = defaultProvider.getExchange("EUR", "TRY");
-//
-//            Exchange exchange2 = forgienExchangeProvider.getExchange("EUR", "TRY");
-//
-//            logger.error("exchange " + exchange.toString());
-//            logger.error("exchange2 " + exchange2.toString());
-//        }
-//    }
+    @Component
+    public static class Runner implements CommandLineRunner
+    {
+
+        private static final Logger logger = LoggerFactory.getLogger("defaultErrorFileLogger");
+        @Autowired
+        @Qualifier( "${forgien_exchange_providers.feign.name}" )
+        private ForgienExchangeProvider defaultProvider;
+
+        @Autowired
+        @Qualifier( "${forgien_exchange_providers.rest.name}" )
+        private ForgienExchangeProvider forgienExchangeProvider;
+
+        //        AllArgsConstructor is not capability to know which qualifier
+        //        @Autowired
+        //        public Runner( @Qualifier( "${forgien_exchange_providers.external.name}" ) ForgienExchangeProvider externalProvider) {
+        //
+        //        }
+
+        @Override
+        public void run( String... args )
+        {
+            Exchange exchange = defaultProvider.getExchange("EUR", "TRY");
+
+            Exchange exchange2 = forgienExchangeProvider.getExchange("EUR", "TRY");
+
+            logger.error("exchange " + exchange.toString());
+            logger.error("exchange2 " + exchange2.toString());
+        }
+    }
 }
